14b85c0fb7cafd7e4645d0e741d49843d465aa65
larrymiami <larrymiami94@gmail.com>
2023-01-27 17:55:37 +0300

    add user id route to retrieve ata from redis

--- routes/users.js
+++ routes/users.js
@@ -28,17 +28,37 @@ router.get("/", function (req, res, _next) {
   res.json(users);
 });
 
+router.get("/:user", (req, res, _next) => {
+  const sessionId = req.cookies.sessionId;
+
+  redis.get(sessionId, (err, result) => {
+    if (err) {
+      res.status(500).send(err);
+    } else {
+      const users = JSON.parse(result);
+
+      const user = users.find((u) => u.id === parseInt(req.params.user));
+      if (user) {
+        res.send(user);
+      } else {
+        res.status(404).send({ message: "User not found" });
+      }
+    }
+  });
+});
+
 /**
  * GET a user using user_id
  */
-router.get("/:user", (req, res, _next) => {
-  const userId = req.params.user;
-  if (userId == 0 || userId > users.length) {
-    res.status(404);
-    res.json("User Not Found");
-    return;
-  }
-  res.json(users[userId - 1]);
-});
+// router.get("/:user", (req, res, _next) => {
+//   console.log("here2");
+//   const userId = req.params.user;
+//   if (userId == 0 || userId > users.length) {
+//     res.status(404);
+//     res.json("User Not Found");
+//     return;
+//   }
+//   res.json(users[userId - 1]);
+// });
 
 module.exports = router;
